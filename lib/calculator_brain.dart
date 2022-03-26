6d2c14c5990efa818939af8ffa891320b71768ab
HI-0123 <hiroki.ito.w@gmail.com>
2022-03-26 14:03:53 +0900

    改行

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -25,11 +25,11 @@ class CalculatorBrain {
 
   String getInterpretation() {
     if (_bmi >= 25) {
-      return '体重が標準を超えてしまっtいるよ。運動や食事に気をつけて！';
+      return '体重が標準を超えてしまっいるよ。\n運動や食事に気をつけて！';
     } else if (_bmi >= 18.5) {
-      return '標準的な体型です。維持していこう！';
+      return '標準的な体型です。\n維持していこう！';
     } else {
-      return '体重が標準を下回っているね。運動や食事に気をつけて！';
+      return '体重が標準を下回っているね。\n運動や食事に気をつけて！';
     }
   }
 }
